a1f61f67e1a75aa6ecf0b5f67c4c714f084f4e81
Patrick <patrick.wang@geekbang.org>
2019-05-13 18:42:12 +0800

    pwa_test

---
+++ dist/precache-manifest.3d5cf4a53a4150b8a5250fddf690d06d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8f41c43ebfa017d3a165",
+    "url": "/js/chunk-vendors.ac21984e.js"
+  },
+  {
+    "revision": "73c8b492b424d44aa014",
+    "url": "/js/app.5d6348a5.js"
+  },
+  {
+    "revision": "8db6726585af3f87dd71",
+    "url": "/js/about.f0c18da7.js"
+  },
+  {
+    "revision": "b21a26897d1c98e60d20bd9db79d0013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "73c8b492b424d44aa014",
+    "url": "/css/app.51421cc3.css"
+  }
+];
